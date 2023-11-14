49f2ba67f4f218bd4e7ce6d6d9eafcfe6b5e07c6
3WAPierre93 <pierre.allileche@3wa.io>
2023-11-14 16:24:03 +0100

    AddFiles

---
+++ Carte.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlayTheSpireProject
+{
+    internal class Carte
+    {
+        //[] Array , liste de cartes 
+        string[] fighters = {"Jin","Yoshimitsu","Christie","Asuka"};
+    }
+
+
+        
+
+}
